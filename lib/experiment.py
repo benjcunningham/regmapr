75e1dfe798ac834aa2d1142b7104cd2bc7990edc
Ben Cunningham <benjamescunningham@gmail.com>
2018-10-10 21:39:12 -0400

    Refactor experiment initialization

--- lib/experiment.py
+++ lib/experiment.py
@@ -1,24 +1,54 @@
-import loss
-import model
+import lib.dataset
+import lib.loss
+import lib.model
+from lib.training import TrainingLoop
 import torch.optim as optim
 
 class Experiment():
 
     def __init__(self, config):
- 
-        model = getattr(model, config["model"])
-        criterion = getattr(loss, config["criterion"])
-        optimizer = getattr(optim, config["optimizer"])
 
         self.config = config
+
+        self.dataset = self.get_dataset(config)
+        vocab_size = len(self.dataset.cols["SENT"].vocab)
+
+        model = self.get_model(vocab_size)
+        criterion = self.get_criterion()
+        optimizer = self.get_optimizer(model.parameters())
+
         self.loop = TrainingLoop(
-            model = model(**config["model_args"]),
-            criterion = criterion(**config["criterion_args"]),
-            optimizer = optimizer(**config["optimizer_args"]),
-            train_phases = config["train_phases"]
+            model=model,
+            criterion=criterion,
+            optimizer=optimizer,
+            train_phases=config["dataset_args"]["train_phases"]
         )
+
+
+    def get_dataset(self, config):
+
+        dataset = getattr(lib.dataset, self.config["dataset"])
+        return dataset(**self.config["dataset_args"])
+
+
+    def get_model(self, vocab_size):
+
+        model = getattr(lib.model, self.config["model"])
+        return model(vocab_size, **self.config["model_args"])
+
+
+    def get_criterion(self):
+
+        criterion = getattr(lib.loss, self.config["criterion"])
+        return criterion(**self.config["criterion_args"])
+
+
+    def get_optimizer(self, params):
+
+        optimizer = getattr(optim, self.config["optimizer"])
+        return optimizer(params, **self.config["optimizer_args"])
 
 
     def run(self):
 
-        self.loop.epoch(config["epochs"], self.dataloaders)
+        self.loop.epoch(config["epochs"], self.dataset)

e5f51f7b06f1be2bf73c20771a93ae28c9f8b26f
Ben Cunningham <benjamescunningham@gmail.com>
2018-10-10 21:48:42 -0400

    Read YAML config

--- train.py
+++ train.py
@@ -1,7 +1,7 @@
 from lib.training import Experiment
 
 def parse_args():
-    
+
     parser = argparse.ArgumentParser()
     parser.add_argument("--config", required=True)
     args = parser.parse_args()
@@ -9,9 +9,18 @@ def parse_args():
     return args
 
 
+def read_yaml(path):
+
+    with open(path, "r") as f:
+        config = yaml.load(f)
+
+    return config
+
+
 if __name__ == "__main__":
 
     args = parse_args()
+    config = read_yaml(args.config)
 
-    exper = Experiment(args.config)
+    exper = Experiment(config)
     exper.run()
